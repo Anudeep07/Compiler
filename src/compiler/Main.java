7dcbbe2a70ee2a8f205992d6cd381fd5560734f5
Anudeep07 <anudeep070199@gmail.com>
2019-03-29 19:26:15 +0530

    finish parser

--- src/compiler/Main.java
+++ src/compiler/Main.java
@@ -6,36 +6,48 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String []args) {
-        if(args.length != 1) {
+    public static void main(String[] args) {
+        if (args.length != 1) {
             System.err.println("Pass file name as first argument.");
             System.exit(0);
         }
 
         try {
             Scanner file = new Scanner(new File(args[0]));
-            file.useDelimiter("\\Z");                       //end of file
+            file.useDelimiter("\\Z"); // end of file
 
             LexAnalyzer lex = new LexAnalyzer();
 
             String source = file.next();
             boolean error = lex.tokenize(source);
 
-            if(!error) {
-                //parse
-            }
-
-            lex.addEOF();
-
-            for(Token token : lex.tokens) {
+            System.out.println("Tokens are: ");
+            for (Token token : lex.tokens) {
                 System.out.println(token);
             }
 
+            if (!error) {
+                Parser p = new Parser(lex.tokens);
 
-        } catch(FileNotFoundException f) {
+                if (p.isvalid())
+                    System.out.println("Parsed successfully!");
+                else {
+                    if (p.ptr <= 0)
+                        p.ptr = 1;
+
+                    if (p.ptr >= p.tokens.size())
+                        p.ptr = p.tokens.size() - 1;
+                    System.err.println("\nError at line " + p.tokens.get(p.ptr - 1).line);
+                }
+
+            }
+
+            file.close();
+        } catch (FileNotFoundException f) {
             System.err.println(args[0] + " couldn't be opened.");
             System.exit(0);
         }
+
     }
 
 }
